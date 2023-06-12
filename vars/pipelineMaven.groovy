1c8ff57afd41bec1cfe4444957b299bbd61d430d
michalsalasa <michal.salasa@onwelo.com>
2023-06-12 12:53:06 +0200

    123123

--- vars/pipelineMaven.groovy
+++ vars/pipelineMaven.groovy
@@ -1,43 +1,35 @@
-// def call(Map settings = [:]) {
-//     node {
-//         agent any
-//         stages {
-//             // stage('Download Source Code') {
-//             //         checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
-                    
-//             // }
-//             // stage('Build') {
-//             //         // Zbudowanie kodu
-//             //         sh 'mvn package -DskipTests'
+def call(Map settings = [:]) {
+    node {
 
-//             // }
-//             // stage('Test') {
-//             //     script {
-//             //         try {
-//             //             if (!settings.skipTests){
-//             //         // Uruchomienie testów aplikacji
-//             //         sh  'mvn verify'
-//             //         // Importowanie wyników testów
-//             //         junit '**/target/surefire-reports/*.xml'
-//             //     }
-//             //         }
-//             //         catch (Exception e) {
-//             //         archiveArtifacts allowEmptyArchieve: true, artifacts: '**/target/surefire-reports/*.xml'
-//             //     }
-//             //     }
-//             // }
-//             stage('Install') {
-//                 steps {
-//                     // Instalowanie artefaktu w lokalnym repozytorium .m2
-//                     // sh 'mvn install -DskipTests'
-//                     echo "test się udał"
-//                 }
-//             }
-//         }
-//     }
-// }
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+            stage('Download Source Code') {
+                    // checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
+                    checkout scm
+            }
+            stage('Build') {
+                    // Zbudowanie kodu
+                    sh 'mvn package -DskipTests'
+
+            }
+            stage('Test') {
+                script {
+                    // try {
+                    //     if (!settings.skipTests){
+                    // Uruchomienie testów aplikacji
+                    sh  'mvn verify'
+                    // Importowanie wyników testów
+                    junit '**/target/surefire-reports/*.xml'
+                
+                    }
+                //     catch (Exception e) {
+                //     archiveArtifacts allowEmptyArchieve: true, artifacts: '**/target/surefire-reports/*.xml'
+                // }
+                }
+            
+            stage('Install') {
+                steps {
+                    // Instalowanie artefaktu w lokalnym repozytorium .m2
+                    sh 'mvn install -DskipTests'
+                }
+        }
+    }
 }
