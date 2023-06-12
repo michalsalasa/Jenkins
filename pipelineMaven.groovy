19b22a4a9c7e963a77f8d07b81a4f1845666f7d4
michalsalasa <michal.salasa@onwelo.com>
2023-06-12 12:12:15 +0200

    prosty pipeline

--- pipelineMaven.groovy
+++ pipelineMaven.groovy
@@ -2,34 +2,35 @@ def call(Map settings = [:]) {
     node {
         agent any
         stages {
-            stage('Download Source Code') {
-                    checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
+            // stage('Download Source Code') {
+            //         checkout ([$class: "GitSCM", branches: [[name: 'main']], userRemoteConfigs:[[url: "https://github.com/michalsalasa/spring-petclinic.git"]]])
                     
-            }
-            stage('Build') {
-                    // Zbudowanie kodu
-                    sh 'mvn package -DskipTests'
+            // }
+            // stage('Build') {
+            //         // Zbudowanie kodu
+            //         sh 'mvn package -DskipTests'
 
-            }
-            stage('Test') {
-                script {
-                    try {
-                        if (!settings.skipTests){
-                    // Uruchomienie testów aplikacji
-                    sh  'mvn verify'
-                    // Importowanie wyników testów
-                    junit '**/target/surefire-reports/*.xml'
-                }
-                    }
-                    catch (Exception e) {
-                    archiveArtifacts allowEmptyArchieve: true, artifacts: '**/target/surefire-reports/*.xml'
-                }
-                }
-            }
+            // }
+            // stage('Test') {
+            //     script {
+            //         try {
+            //             if (!settings.skipTests){
+            //         // Uruchomienie testów aplikacji
+            //         sh  'mvn verify'
+            //         // Importowanie wyników testów
+            //         junit '**/target/surefire-reports/*.xml'
+            //     }
+            //         }
+            //         catch (Exception e) {
+            //         archiveArtifacts allowEmptyArchieve: true, artifacts: '**/target/surefire-reports/*.xml'
+            //     }
+            //     }
+            // }
             stage('Install') {
                 steps {
                     // Instalowanie artefaktu w lokalnym repozytorium .m2
-                    sh 'mvn install -DskipTests'
+                    // sh 'mvn install -DskipTests'
+                    echo "test się udał"
                 }
             }
         }
